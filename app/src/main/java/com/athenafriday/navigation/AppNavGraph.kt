7177f179368a6b6b60f781694bd5f65ad3514621
Sai Bighnesh <saibighneshprusty@gmail.com>
2025-04-30 07:29:05 -0400

    Resolved the navigation error and was able to work on the handyman application

--- app/src/main/java/com/athenafriday/navigation/AppNavGraph.kt
+++ app/src/main/java/com/athenafriday/navigation/AppNavGraph.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.athenafriday.handyman.navigation.HandymanNavHost
 import com.athenafriday.screens.DashboardScreen
 import com.athenafriday.tinderjerome.navigation.TinderNavHost
 
@@ -42,7 +43,7 @@ fun AppNavGraph(navController: NavHostController) {
 //        composable(Destinations.CHAT) { ChatScreen(navController) }
 //        composable(Destinations.DELIVERY) { DeliveryScreen(navController) }
 //        composable(Destinations.EAT) { EatScreen(navController) }
-//        composable(Destinations.HANDYMAN) { HandymanScreen(navController) }
+            composable(Destinations.HANDYMAN) { HandymanNavHost() }
 //        composable(Destinations.IBANK) { IbankScreen(navController) }
 //        composable(Destinations.LEARN) { LearnScreen(navController) }
 //        composable(Destinations.PC_REPAIR) { PCRepairScreen(navController) }
